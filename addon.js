5eee2776c37f5cc6360831d61171ef67f8c9fe2c
Michael Kohler <mkohler@picobudget.com>
2015-07-21 22:59:16 +0200

    cleanup + show percentage instantly

--- addon.js
+++ addon.js
@@ -11,34 +11,31 @@
   else {
     window.addEventListener('DOMContentLoaded', initialize);
   }
-
-  var statusBarEl;
+  
   var containerEl;
-  var chargingPercentage = 0;
 
   function initialize() {
+    var battery = window.navigator.battery;
     
     // Get the status bar bar
-    statusBarEl = document.getElementById('statusbar-maximized');
+    var statusBarEl = document.getElementById('statusbar-maximized');
 
     // If there is a old one, remove it first
-    containerEl = document.getElementById('statusbar-battery');
+    containerEl = document.getElementById('statusbar-batterypercentage');
     if (statusBarEl.contains(containerEl)) {
       statusBarEl.removeChild(containerEl);
     }
     
     // Build the battery percentage element
     containerEl = document.createElement('div');
-    containerEl.setAttribute('id', 'statusbar-battery');
-    containerEl.style.order = '-2';
+    containerEl.setAttribute('id', 'statusbar-batterypercentage');
+    containerEl.style.order = '-3';
     containerEl.style.fontSize = '1.5rem';
     containerEl.style.fontWeight = '400';
     containerEl.style.lineHeight = '1.6rem';
-    containerEl.textContent = '---';
+    containerEl.textContent = battery.level * 100 + '%';
     
     statusBarEl.appendChild(containerEl);
-    
-    var battery = window.navigator.battery;
    
     battery.addEventListener('levelchange', function() {
       containerEl.textContent = battery.level * 100 + '%';
