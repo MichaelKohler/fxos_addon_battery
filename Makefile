6661c808bfd143e6fb881d26b687c141d5141d93
Michael Kohler <mkohler@picobudget.com>
2015-10-18 02:51:19 +0200

    update battery status to new format and fix float issue

---
+++ Makefile
@@ -0,0 +1,2 @@
+package:
+	zip package.zip index.js manifest.json 128.png
--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 (function () {
   // If injecting into an app that was already running at the time
-  // the app was enabled, simply initialize it.
+  // the app was enabled, simply initialize it. Otherwise initialize it after it
+  // is loaded.
   if (document.documentElement) {
     initialize();
-  }
-
-  // Otherwise, we need to wait for the DOM to be ready before
-  // starting initialization since add-ons are usually (always?)
-  // injected *before* `document.documentElement` is defined.
-  else {
+  } else {
     window.addEventListener('DOMContentLoaded', initialize);
   }
-  
+
   var containerEl;
 
   function initialize() {
     var battery = window.navigator.battery;
-    
-    // Get the status bar bar
     var statusBarEl = document.getElementById('statusbar-maximized');
-
-    // If there is a old one, remove it first
     containerEl = document.getElementById('statusbar-batterypercentage');
+
     if (statusBarEl.contains(containerEl)) {
       statusBarEl.removeChild(containerEl);
     }
-    
+
     // Build the battery percentage element
     containerEl = document.createElement('div');
     containerEl.setAttribute('id', 'statusbar-batterypercentage');
@@ -33,12 +26,12 @@
     containerEl.style.fontSize = '1.5rem';
     containerEl.style.fontWeight = '400';
     containerEl.style.lineHeight = '1.6rem';
-    containerEl.textContent = battery.level * 100 + '%';
-    
+    containerEl.textContent = Math.floor(battery.level * 100) + '%';
+
     statusBarEl.appendChild(containerEl);
-   
+
     battery.addEventListener('levelchange', function() {
-      containerEl.textContent = battery.level * 100 + '%';
+      containerEl.textContent = Math.floor(battery.level * 100) + '%';
     });
   }
 }());
